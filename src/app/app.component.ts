4dd9d30a62faa7a32c6027ee80b7dfcad19db524
Antoine MENGUY-MANCEAU <antoine.menguymanceau@orange.com>
2024-03-25 18:47:55 +0100

    [ponyracer-main][update]Add menu component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from "./menu/menu.component";
 
 @Component({
   selector: 'pr-root',
   standalone: true,
-  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [MenuComponent]
 })
 export class AppComponent {
   title = 'ponyracer';
