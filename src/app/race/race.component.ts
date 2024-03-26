c1f4cce865b2f7070e76016b6fb0fc431496f9d5
Antoine MENGUY-MANCEAU <antoine.menguymanceau@orange.com>
2024-03-26 13:57:30 +0100

    [ponyracer-main][feat] Add datepipe

--- src/app/race/race.component.ts
+++ src/app/race/race.component.ts
@@ -2,13 +2,14 @@
 import { Component, Input } from '@angular/core';
 import { RaceModel } from '../models/race.model';
 import { PonyComponent } from "../pony/pony.component";
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'pr-race',
   standalone: true,
   templateUrl: './race.component.html',
   styleUrl: './race.component.css',
-  imports: [PonyComponent]
+  imports: [PonyComponent, DatePipe]
 })
 export class RaceComponent {
   @Input({ required: true }) raceModel!: RaceModel;
