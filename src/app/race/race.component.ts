aa70632f9756e1c567e88745052d1f5e9c62647e
Antoine MENGUY-MANCEAU <antoine.menguymanceau@orange.com>
2024-03-26 12:22:12 +0100

    [ponyracer-main][feat] Add poyn detail

--- src/app/race/race.component.ts
+++ src/app/race/race.component.ts
@@ -1,13 +1,14 @@
 /* eslint-disable prettier/prettier */
 import { Component, Input } from '@angular/core';
 import { RaceModel } from '../models/race.model';
+import { PonyComponent } from "../pony/pony.component";
 
 @Component({
   selector: 'pr-race',
   standalone: true,
-  imports: [],
   templateUrl: './race.component.html',
-  styleUrl: './race.component.css'
+  styleUrl: './race.component.css',
+  imports: [PonyComponent]
 })
 export class RaceComponent {
   @Input({ required: true }) raceModel!: RaceModel;
